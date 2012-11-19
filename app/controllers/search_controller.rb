6e144cb8602c4b4e9fa83aad38aa6ace15e98096
David Kitchener <kitchener.david@gmail.com>
2012-11-18 20:02:22 -0800

    create pagination partial and search mock

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+	page = params.has_key?(:page)  ? params[:page] : 1
+	search_term = params.has_key?(:term) ? params[:page] : "Fender"
+	@search= Product.search do
+		fulltext ("Fender")
+		paginate :page => page, :per_page => 3
+	end
+	puts @search.total	
   end
 end
