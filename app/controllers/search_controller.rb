ef1eac94baf4c67b1d0d3c1d77f7d4ed6d04f795
David Kitchener <kitchener.david@gmail.com>
2012-11-18 23:30:32 -0800

    wire in search field on home

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,10 @@
 class SearchController < ApplicationController
   def index
 	page = params.has_key?(:page)  ? params[:page] : 1
-	search_term = params.has_key?(:term) ? params[:page] : "Fender"
+	search_term = params.has_key?(:term) ? params[:term] : ''
 	@search= Product.search do
-		fulltext ("Fender")
+		fulltext (search_term)
 		paginate :page => page, :per_page => 3
 	end
-	puts @search.total	
   end
 end
